6a94a25907c48c9a3f83e8d00332f38be16985ce
Pedro Daniel <pedro.martinez@modalgr.io>
2025-02-24 14:46:39 -0300

    feat: update method

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -19,6 +19,11 @@ export class PensamentoService {
     return this.http.post<Pensamento>(this.API_URL, pensamento);
   }
 
+  editar(pensamento: Pensamento): Observable<Pensamento> {
+    const url = `${this.API_URL}/${pensamento.id}`;
+    return this.http.put<Pensamento>(url, pensamento);
+  }
+
   excluir(id: number): Observable<Pensamento> {
     const url = `${this.API_URL}/${id}`;
     return this.http.delete<Pensamento>(url);
